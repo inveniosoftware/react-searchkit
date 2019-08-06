af7aa654a15786607ebb64492f63546cd1be5b88
vladbmihaescu <vladbmihaescu@protonmail.ch>
2019-08-06 17:56:41 +0200

    components: add HOC component to read query state
    Co-authored-by: zzacharo <zacharias.zacharodimos@cern.ch>

---
+++ src/components/HOC/index.js
@@ -0,0 +1,1 @@
+export { withQueryState } from './withQueryState';
---
+++ src/components/HOC/withQueryState.js
@@ -0,0 +1,23 @@
+/*
+ * This file is part of React-SearchKit.
+ * Copyright (C) 2019 CERN.
+ *
+ * React-SearchKit is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import React from 'react';
+import { connect } from '@app/store';
+
+export function withQueryState(Component) {
+  const WrappedComponent = ({ dispatch, ...props }) => <Component {...props} />;
+  const mapStateToProps = state => ({
+    currentQueryState: state.query,
+    currentResultsState: state.results,
+  });
+
+  return connect(
+    mapStateToProps,
+    null
+  )(WrappedComponent);
+}
