1d029c1a64545578ab5b623a880669a4e4025a12
Georgios Lignos <glignos93@gmail.com>
2020-10-27 11:40:24 +0100

    components: fix toggle components overridability

--- src/lib/components/Toggle/Toggle.js
+++ src/lib/components/Toggle/Toggle.js
@@ -13,58 +13,64 @@ import _get from 'lodash/get';
 import { Card } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-class ToggleComponent extends Component{
+class ToggleComponent extends Component {
 
-      _isChecked = (userSelectionFilters) => {
-        const isFilterActive = userSelectionFilters.filter(
-            (filter) => filter[0] === this.props.filterValue[0]
-          ).length > 0
-        return isFilterActive
-      };
-    
-      onToggleClicked = () => {
-        this.props.updateQueryFilters(this.props.filterValue);
-      };
-      
-      render() {
-        const {
-            userSelectionFilters,
-            overridableId,
-            ...props
-          } = this.props;
-        var isChecked = this._isChecked(userSelectionFilters);
-        return(
-        <Overridable id={'SearchFilters.ToggleComponent', this.props.overridableId} {...this.props}>
-            
-            <Card>
-                <Card.Content>
-                    <Card.Header>{this.props.title}</Card.Header>
-                </Card.Content>
-                <Card.Content>
-                    <Checkbox
-                        toggle
-                        label={this.props.label}
-                        onClick={this.onToggleClicked}
-                        checked={isChecked}
-                    />
-                </Card.Content>
-            </Card>
-        </Overridable>
-        )
-      }
+  _isChecked = (userSelectionFilters) => {
+    const isFilterActive = userSelectionFilters.filter(
+      (filter) => filter[0] === this.props.filterValue[0]
+    ).length > 0
+    return isFilterActive
+  };
+
+  onToggleClicked = () => {
+    this.props.updateQueryFilters(this.props.filterValue);
+  };
+
+  render() {
+    const {
+      userSelectionFilters,
+      overridableId,
+      title,
+      label
+    } = this.props;
+    var isChecked = this._isChecked(userSelectionFilters);
+    const onToggleClicked = this.onToggleClicked
+    debugger
+    return (
+      <Overridable
+       id={'SearchFilters.ToggleComponent', overridableId}
+       isChecked={isChecked}
+       onToggleClicked={onToggleClicked}
+       {...this.props}>
+        <Card>
+          <Card.Content>
+            <Card.Header>{title}</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <Checkbox
+              toggle
+              label={label}
+              onClick={onToggleClicked}
+              checked={isChecked}
+            />
+          </Card.Content>
+        </Card>
+      </Overridable>
+    )
+  }
 }
 
 ToggleComponent.propTypes = {
-    title: PropTypes.string,
-    label: PropTypes.string,
-    userSelectionFilters: PropTypes.array.isRequired,
-    updateQueryFilters: PropTypes.func.isRequired,
-    overridableId: PropTypes.string,
-  };
-  
-  ToggleComponent.defaultProps = {
-    overridableId: '',
-  };
+  title: PropTypes.string,
+  label: PropTypes.string,
+  userSelectionFilters: PropTypes.array.isRequired,
+  updateQueryFilters: PropTypes.func.isRequired,
+  overridableId: PropTypes.string,
+};
+
+ToggleComponent.defaultProps = {
+  overridableId: '',
+};
 
 export default Overridable.component(
-    'SearchFilters.ToggleComponent', ToggleComponent);
+  'SearchFilters.ToggleComponent', ToggleComponent);
