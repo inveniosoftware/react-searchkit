5cc7903d0ac640e918a6f93b343070c579b0de6a
inveniobot <info@inveniosoftware.org>
2026-08-28 08:01:42 +0000

    Deploy website
    Deploy website version based on 23962fecb18950ae9b6bde4a2922437998755fcc

--- js/code-blocks-buttons.js
+++ js/code-blocks-buttons.js
@@ -1,9 +1,6 @@
 /*
- * This file is part of React-SearchKit.
- * Copyright (C) 2018 CERN.
- *
- * React-SearchKit is free software; you can redistribute it and/or modify it
- * under the terms of the MIT License; see LICENSE file for more details.
+ * SPDX-FileCopyrightText: 2018 CERN.
+ * SPDX-License-Identifier: MIT
  */
 
 // Turn off ESLint for this file because it's sent down to users as-is.
